b6012a057c664a9fab1f20d211a6e7f6e0c2ec47
afiqdev-go <mesminachannel31@gmail.com>
2024-09-07 10:15:15 +0700

    Refactor midtrans.config.js and hide sensitive information

--- app/config/midtrans.config.js
+++ app/config/midtrans.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    midtrans_server_key: "SB-Mid-server-smqJLZjjT2y9xLjGQk7CbrOU",
+    midtrans_server_key: "***",
     midtrans_app_url: "https://app.sandbox.midtrans.com",
     frontend_url: "http://localhost:3000",
 }
